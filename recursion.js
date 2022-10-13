c6a2e8f0e3f489837ed26c65774c6d0f25c78f1d
john katua <johnkatua99@gmail.com>
2022-10-13 14:36:33 +0300

    fixed the recursion function

--- recursion.js
+++ recursion.js
@@ -1,6 +1,6 @@
 const fizzBuzz = num => {
   // this is the base case to avoid infinite loop
-  if (num >= 100) {
+  if (num > 100) {
     return;
   };
   if (num % 3 === 0 && num % 5 === 0) {
@@ -8,7 +8,7 @@ const fizzBuzz = num => {
   } else if (num % 3 === 0) {
     console.log('Fizz');
   } else if (num % 5 === 0) {
-    console.log('Buzz');
+    console.log(num, 'Buzz');
   }
   fizzBuzz(num + 1);
 };
